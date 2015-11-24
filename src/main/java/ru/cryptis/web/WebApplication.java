c5f191895737cd3d4f42e35efd7decba771e338d
Pavel Vakhrushev <webartifex@gmail.com>
2015-11-24 20:37:30 +0500

    production ready

--- src/main/java/ru/cryptis/web/WebApplication.java
+++ src/main/java/ru/cryptis/web/WebApplication.java
@@ -2,10 +2,24 @@ package ru.cryptis.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WebApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurerAdapter enableEnglishVersion() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/en").setViewName("forward:/en/index.html");
+            }
+        };
     }
 }
